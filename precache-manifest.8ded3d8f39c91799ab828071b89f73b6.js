ee93a859798fa35bd9f19b84b91c60044dacb109
GoodBoiDoggo <yadao.meynardkhel@gmail.com>
2019-06-04 11:24:40 +0800

    Release 1.0

---
+++ precache-manifest.8ded3d8f39c91799ab828071b89f73b6.js
@@ -0,0 +1,346 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f79b0b2a42cf5899fa421d505a678f4",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled_min5/25_redundant_orientation_confusion_matrix_TOPCONS2.png"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/TMEval-Frontend/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "cf29878d31aa702e0b04",
+    "url": "/TMEval-Frontend/js/chunk-2d0e8bc5.2ce6d524.js"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/TMEval-Frontend/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "9744d87832d3f825b108",
+    "url": "/TMEval-Frontend/js/chunk-ebdaf33a.874cbc9e.js"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/TMEval-Frontend/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "da71d4d699be20b5b596",
+    "url": "/TMEval-Frontend/js/chunk-vendors.3f32dc37.js"
+  },
+  {
+    "revision": "a56c5a04eb924f4497b0",
+    "url": "/TMEval-Frontend/js/app.138d95fd.js"
+  },
+  {
+    "revision": "d7e0f437e7a2c0c6d4d914ea17365954",
+    "url": "/TMEval-Frontend/index.html"
+  },
+  {
+    "revision": "3599927c753825c5e1126077b1ebb99d",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled/25_redundant_orientation_confusion_matrix_CCTOP.png"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/TMEval-Frontend/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/TMEval-Frontend/img/nucleo-icons.46abbc4a.svg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/TMEval-Frontend/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/TMEval-Frontend/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/TMEval-Frontend/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/TMEval-Frontend/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/TMEval-Frontend/img/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/TMEval-Frontend/img/theme/profile.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/TMEval-Frontend/img/theme/landing.jpg"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/TMEval-Frontend/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/TMEval-Frontend/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/TMEval-Frontend/img/brand/favicon.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/TMEval-Frontend/img/brand/white.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/TMEval-Frontend/img/brand/blue.png"
+  },
+  {
+    "revision": "a957f7e82403ef5c1efe54e4085e8196",
+    "url": "/TMEval-Frontend/img/brand/tmbrand.png"
+  },
+  {
+    "revision": "76552b31a29e04d063f8e5b6aa7230aa",
+    "url": "/TMEval-Frontend/img/figures/bar_protein_count_tx_all.png"
+  },
+  {
+    "revision": "64a293e13a1f1d1e5b27fb2aa8805685",
+    "url": "/TMEval-Frontend/img/figures/CCTOP.png"
+  },
+  {
+    "revision": "6c908a72273812db3d24a19c9e2bc3c1",
+    "url": "/TMEval-Frontend/img/figures/hmmtop.png"
+  },
+  {
+    "revision": "6aa2cf3dce01fc33e2979286d49c2e84",
+    "url": "/TMEval-Frontend/img/figures/TMHMM2.png"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/TMEval-Frontend/img/theme/promo-1.png"
+  },
+  {
+    "revision": "e7b99801bd22faba8ba85e301e973f84",
+    "url": "/TMEval-Frontend/img/figures/bar_tm_helix_length_25.png"
+  },
+  {
+    "revision": "d407b8b7666f7d48ce20b0b83812004c",
+    "url": "/TMEval-Frontend/img/figures/bar_tm_length_25.png"
+  },
+  {
+    "revision": "f5ea120bc44f7819b330ad30ad578dbb",
+    "url": "/TMEval-Frontend/img/figures/philius.png"
+  },
+  {
+    "revision": "d9a731e080ba39ce281ddd6b5367822e",
+    "url": "/TMEval-Frontend/img/boi.png"
+  },
+  {
+    "revision": "d1ffefdb42d745c7851206f63411a482",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/25_redundant_orientation_confusion_matrix_CCTOP.png"
+  },
+  {
+    "revision": "e8b354035da3d97316301033bafbe5a0",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/25_redundant_orientation_confusion_matrix_HMMTOP.png"
+  },
+  {
+    "revision": "995b703c1c6e8246e8404bbd5cd58266",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/25_redundant_orientation_confusion_matrix_PHILIUS.png"
+  },
+  {
+    "revision": "51bb5c786d1f50c7f5b7caae7e77cd39",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/25_redundant_orientation_confusion_matrix_TMHMM2.png"
+  },
+  {
+    "revision": "cf415ff54d8f2e74e2181d5f69d00fe8",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/25_redundant_orientation_confusion_matrix_TOPCONS2.png"
+  },
+  {
+    "revision": "f19c5779ff1685736358749ee40c9737",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled_min5/25_redundant_orientation_confusion_matrix_PHILIUS.png"
+  },
+  {
+    "revision": "3599927c753825c5e1126077b1ebb99d",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled_min5/25_redundant_orientation_confusion_matrix_CCTOP.png"
+  },
+  {
+    "revision": "338dd0430dbae13179c4f8b25b097cc6",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled_min5/25_redundant_orientation_confusion_matrix_HMMTOP.png"
+  },
+  {
+    "revision": "3104b62a2bfde05c98351dc3c31fac9f",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled_min5/25_redundant_orientation_confusion_matrix_TMHMM2.png"
+  },
+  {
+    "revision": "77b8032481c30c602134fbf82ae8db44",
+    "url": "/TMEval-Frontend/img/figures/cm_redundant/25_redundant_classification_confusion_matrix_TOPCONS2.png"
+  },
+  {
+    "revision": "338dd0430dbae13179c4f8b25b097cc6",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled/25_redundant_orientation_confusion_matrix_HMMTOP.png"
+  },
+  {
+    "revision": "3104b62a2bfde05c98351dc3c31fac9f",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled/25_redundant_orientation_confusion_matrix_TMHMM2.png"
+  },
+  {
+    "revision": "2f79b0b2a42cf5899fa421d505a678f4",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled/25_redundant_orientation_confusion_matrix_TOPCONS2.png"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/TMEval-Frontend/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "f19c5779ff1685736358749ee40c9737",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled/25_redundant_orientation_confusion_matrix_PHILIUS.png"
+  },
+  {
+    "revision": "1c35bd23f0fc68af7f64fbad7ee0e3aa",
+    "url": "/TMEval-Frontend/img/figures/predtools.png"
+  },
+  {
+    "revision": "3c0bed7a7e715e6281b5d36617cb6879",
+    "url": "/TMEval-Frontend/img/figures/TOPCONS.png"
+  },
+  {
+    "revision": "fbac946f2584bbdc9e7a2540e6be5531",
+    "url": "/TMEval-Frontend/img/figures/bar_mode_organisms_25.png"
+  },
+  {
+    "revision": "a5ec6fb4837138a1436c3d3ed3714924",
+    "url": "/TMEval-Frontend/img/figures/cm_redundant/25_redundant_orientation_confusion_matrix_CCTOP.png"
+  },
+  {
+    "revision": "2790253a961883967f5f65008205978b",
+    "url": "/TMEval-Frontend/img/figures/cm_redundant/25_redundant_orientation_confusion_matrix_HMMTOP.png"
+  },
+  {
+    "revision": "838fdda31e37411adc2f182fb6108eac",
+    "url": "/TMEval-Frontend/img/figures/cm_redundant/25_redundant_orientation_confusion_matrix_PHILIUS.png"
+  },
+  {
+    "revision": "d483433662e076b92ad9bf066e1c1f65",
+    "url": "/TMEval-Frontend/img/figures/cm_redundant/25_redundant_orientation_confusion_matrix_TMHMM2.png"
+  },
+  {
+    "revision": "6f83908aca610cd3d5ab1aa0d87f4cf8",
+    "url": "/TMEval-Frontend/img/figures/cm_redundant/25_redundant_orientation_confusion_matrix_TOPCONS2.png"
+  },
+  {
+    "revision": "59e75c0d3e8528a6eff0a6c139e1fe23",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/25_redundant_classification_confusion_matrix_HMMTOP.png"
+  },
+  {
+    "revision": "bd2aaf6f6513184c67b3260035d304c2",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled_min5/25_redundant_classification_confusion_matrix_TMHMM2.png"
+  },
+  {
+    "revision": "bd2aaf6f6513184c67b3260035d304c2",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled/25_redundant_classification_confusion_matrix_TMHMM2.png"
+  },
+  {
+    "revision": "b2ce28da2d44be53a0d1c161b7474aea",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/25_redundant_classification_confusion_matrix_CCTOP.png"
+  },
+  {
+    "revision": "380ccfee2d06229aa20e883554fda58b",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/25_redundant_classification_confusion_matrix_PHILIUS.png"
+  },
+  {
+    "revision": "c76f084a12473441ec83e5994b40035a",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/25_redundant_classification_confusion_matrix_TMHMM2.png"
+  },
+  {
+    "revision": "57730f0905d11b61f422c898e8b33a1a",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/25_redundant_classification_confusion_matrix_TOPCONS2.png"
+  },
+  {
+    "revision": "0051477b9e712800ec56f01067296e0c",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled_min5/25_redundant_classification_confusion_matrix_CCTOP.png"
+  },
+  {
+    "revision": "07b3419c145945ebe0c039f889e8735e",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled_min5/25_redundant_classification_confusion_matrix_HMMTOP.png"
+  },
+  {
+    "revision": "1696a98973e4d1e3ca8bfd1d898d4655",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled_min5/25_redundant_classification_confusion_matrix_PHILIUS.png"
+  },
+  {
+    "revision": "e4ddb5f3ebe144c511b8b508ad361690",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled_min5/25_redundant_classification_confusion_matrix_TOPCONS2.png"
+  },
+  {
+    "revision": "1696a98973e4d1e3ca8bfd1d898d4655",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled/25_redundant_classification_confusion_matrix_PHILIUS.png"
+  },
+  {
+    "revision": "07b3419c145945ebe0c039f889e8735e",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled/25_redundant_classification_confusion_matrix_HMMTOP.png"
+  },
+  {
+    "revision": "e4ddb5f3ebe144c511b8b508ad361690",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled/25_redundant_classification_confusion_matrix_TOPCONS2.png"
+  },
+  {
+    "revision": "9fc666873ae488e5a0ac178e5219f8fa",
+    "url": "/TMEval-Frontend/img/figures/cm_redundant/25_redundant_classification_confusion_matrix_CCTOP.png"
+  },
+  {
+    "revision": "e9682c85601f7f750129e8850932eaba",
+    "url": "/TMEval-Frontend/img/figures/cm_redundant/25_redundant_classification_confusion_matrix_PHILIUS.png"
+  },
+  {
+    "revision": "0051477b9e712800ec56f01067296e0c",
+    "url": "/TMEval-Frontend/img/figures/cm_nonredundant/resampled/25_redundant_classification_confusion_matrix_CCTOP.png"
+  },
+  {
+    "revision": "6b221316a34de743eb2acc4d62293e84",
+    "url": "/TMEval-Frontend/img/figures/cm_redundant/25_redundant_classification_confusion_matrix_TMHMM2.png"
+  },
+  {
+    "revision": "70f23c816f50b5a700c688a8ff24193e",
+    "url": "/TMEval-Frontend/img/figures/cm_redundant/25_redundant_classification_confusion_matrix_HMMTOP.png"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/TMEval-Frontend/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/TMEval-Frontend/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/TMEval-Frontend/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/TMEval-Frontend/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/TMEval-Frontend/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/TMEval-Frontend/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/TMEval-Frontend/favicon.png"
+  },
+  {
+    "revision": "da71d4d699be20b5b596",
+    "url": "/TMEval-Frontend/css/chunk-vendors.2d73a294.css"
+  },
+  {
+    "revision": "9744d87832d3f825b108",
+    "url": "/TMEval-Frontend/css/chunk-ebdaf33a.62b24daf.css"
+  },
+  {
+    "revision": "a56c5a04eb924f4497b0",
+    "url": "/TMEval-Frontend/css/app.412ce22c.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/TMEval-Frontend/precache-manifest.a6b4a50b1c9101deb58d6041fdf36748.js"
+  "/TMEval-Frontend/precache-manifest.8ded3d8f39c91799ab828071b89f73b6.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "tmeval-frontend"});
